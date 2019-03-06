b61eb633b198599c4fdd2540a6e220ced1757b57
ywlqjl <yanwenliqjl@gmail.com>
2019-03-06 11:25:50 +0100

    modify servo_5

--- tp/SERVO_V5/SERVO_V5.ino
+++ tp/SERVO_V5/SERVO_V5.ino
@@ -15,23 +15,22 @@ void setup()
   Servo_droit.attach(SERVO_DROIT_PIN);  
   Servo_gauche.attach(SERVO_GAUCHE_PIN);
 
+  //1. Faire tourner les deux servo moteurs dans le sens horaire pendant 3s ;
   Servo_droit.writeMicroseconds (T1_1);
   Servo_gauche.writeMicroseconds (T1_1);
   delay (3000);
-
+  //2. Faire tourner les deux servo moteurs dans le sens trigonométrique pendant 2.5s
   Servo_droit.writeMicroseconds (T1_2);
   Servo_gauche.writeMicroseconds (T1_2);
+  //3. Arrêter les deux servo moteurs.
   delay (2500);
   
 //  Servo_droit.writeMicroseconds (T1_3);
 //  Servo_gauche.writeMicroseconds (T1_3);
     Servo_droit.detach();
     Servo_gauche.detach();
-
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-
 }
