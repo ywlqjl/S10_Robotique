5f9739ab01f91bc618d7320cf4c1a04d4d14f629
ywlqjl <yanwenliqjl@gmail.com>
2019-03-28 13:45:08 +0100

    Pivoter

---
+++ tp/Pivoter_Droite_V1/Pivoter_Droite_V1.ino
@@ -0,0 +1,65 @@
+#include <Servo.h>
+
+//Définition de constantes symboliques
+#define BUZZER_PIN 4
+#define SERVO_GAUCHE_PIN 11
+#define SERVO_DROIT_PIN 10
+#define DUREE 6000
+
+// Variables globales
+Servo Servo_droit;
+Servo Servo_gauche;
+
+int largeur_implusion;
+
+void setup() 
+{
+  tone(BUZZER_PIN, 3000);
+  delay(1000);
+  noTone(BUZZER_PIN);
+  
+  Serial.begin(9600);
+  Servo_gauche.attach(SERVO_GAUCHE_PIN);  
+  Servo_gauche.writeMicroseconds (1500);
+  Servo_droit.attach(SERVO_DROIT_PIN);  
+  Servo_droit.writeMicroseconds (1500);
+  Droite(6000);
+  arret();
+  
+}
+
+void loop() {
+
+}
+
+
+
+void Avance (int duree_deplacement) 
+{
+  Servo_droit.writeMicroseconds(1700); 
+  Servo_gauche.writeMicroseconds(1300); 
+  delay (duree_deplacement);
+}
+
+void arret (void) 
+{
+  Servo_droit.detach (); 
+  Servo_gauche.detach ();
+}
+
+
+void Recule (int duree_deplacement) 
+{
+  Servo_droit.writeMicroseconds(1300); 
+  Servo_gauche.writeMicroseconds(1700); 
+  delay (duree_deplacement);
+}
+
+
+
+void Droite (int duree_deplacement) 
+{
+  Servo_droit.writeMicroseconds(1700); 
+  Servo_gauche.writeMicroseconds(1700); 
+  delay (duree_deplacement);
+}
